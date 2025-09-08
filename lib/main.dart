3d73b4a0c1659c2fd32e85792a09ec27f768501f
Nour Hagag <nourhgag159@gmail.com>
2025-09-08 17:41:31 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ImageCard());
+}
+
+class ImageCard extends StatelessWidget {
+  const ImageCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Image.asset(fit: BoxFit.cover, 'assets/images/image2.jpg'),
+        ),
+      ),
+    );
+  }
+}
